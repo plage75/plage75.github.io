b3adf51514ebdcc9711ce015b25f558efb1ff220
plage75 <mataba75@gmail.com>
2019-12-24 17:58:56 +0300

    Add files via upload

---
+++ app/js/common.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+//toggle
+$(".toggle-mnu").click(function() {
+	$(this).toggleClass("on");
+	$(".main-mnu").slideToggle();
+	return false;
+});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$(".forms").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+				$(".forms").trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
